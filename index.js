ac6feeadb64f1a7f5a6613d9a7f22f15535759c4
GabriellaFsena <gabriellasenatech@gmail.com>
2023-10-27 15:26:47 -0300

    atualizações

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    $(".carousel").slick();
+$(document).ready(function () {
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,  
+    });
+});
+
+
+// Adicione um evento de clique para alternar a exibição da lista no ícone do menu
+document.querySelector('.menu-icon').addEventListener('click', function () {
+    var menuList = document.querySelector('.menu-list');
+    menuList.classList.toggle('change');
 });
